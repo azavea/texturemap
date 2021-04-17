bd91fec277c3c9ed7e46eec0b30bf595a4d2a746
jfrankl <jfrankl@azavea.com>
2021-04-17 17:54:42 -0400

    Add additional shapes

--- src/index.js
+++ src/index.js
@@ -14,16 +14,66 @@ const types = [
   {
     name: "points",
     styles: [
-      { name: "blue", color: "#1f77b4" },
-      { name: "orange", color: "#ff7f0e" },
-      { name: "green", color: "#2ca02c" },
-      { name: "red", color: "#d62728" },
-      { name: "purple", color: "#9467bd" },
-      { name: "brown", color: "#8c564b" },
-      { name: "pink", color: "#e377c2" },
-      { name: "grey", color: "#333" },
-      { name: "slime", color: "#bcbd22" },
-      { name: "teal", color: "#17becf" },
+      {
+        name: "blue",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#1f77b4",
+      },
+      {
+        name: "orange",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#ff7f0e",
+      },
+      {
+        name: "green",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#2ca02c",
+      },
+      {
+        name: "red",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#d62728",
+      },
+      {
+        name: "purple",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#9467bd",
+      },
+      {
+        name: "brown",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#8c564b",
+      },
+      {
+        name: "pink",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#e377c2",
+      },
+      {
+        name: "grey",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#333",
+      },
+      {
+        name: "slime",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#bcbd22",
+      },
+      {
+        name: "teal",
+        strokeColor: "#fff",
+        strokeWidth: "1.5",
+        fillColor: "#17becf",
+      },
     ],
   },
 ];
@@ -34,26 +84,32 @@ function generateSVGs({ name, styles }) {
   fs.mkdirSync(output, { recursive: true });
   const files = fs.readdirSync(input);
 
-  styles.forEach(({ name: colorName, color: colorHex }) => {
-    files.forEach((filename) => {
-      const oldFile = `${input}/${filename}`;
-      const newFile = `${output}/${filename.replace(
-        ".svg",
-        `-${colorName}.svg`
-      )}`;
+  styles.forEach(
+    ({
+      name,
+      fillColor = "#000",
+      strokeColor = "#fff",
+      strokeWidth = "1.5",
+    }) => {
+      files.forEach((filename) => {
+        const oldFile = `${input}/${filename}`;
+        const newFile = `${output}/${filename.replace(".svg", `-${name}.svg`)}`;
 
-      fs.readFile(oldFile, "utf8", function (err, data) {
-        if (err) {
-          return console.log(err);
-        }
-        var result = data.replace(/%COLOR%/g, colorHex);
+        fs.readFile(oldFile, "utf8", function (err, data) {
+          if (err) {
+            return console.log(err);
+          }
+          data = data.replace(/%FILLCOLOR%/g, fillColor);
+          data = data.replace(/%STROKECOLOR%/g, strokeColor);
+          data = data.replace(/%STROKEWIDTH%/g, strokeWidth);
 
-        fs.writeFile(newFile, result, "utf8", function (err) {
-          if (err) return console.log(err);
+          fs.writeFile(newFile, data, "utf8", function (err) {
+            if (err) return console.log(err);
+          });
         });
       });
-    });
-  });
+    }
+  );
 }
 
 try {
