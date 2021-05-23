9b4572998523807b83ab90e2d42f6753e73a7167
jfrankl <jfrankl@azavea.com>
2021-05-23 07:56:57 -0400

    Another attempt to copy examples

--- src/copy-examples.js
+++ src/copy-examples.js
@@ -1,6 +1,15 @@
 var fs = require("fs-extra");
 
-fs.copy("examples", "website/examples", function(err) {
-  if (err) return console.error(err);
-  console.log("Success: examples copied to website directory");
-});
+try {
+  fs.copy("website", "_site", function(err) {
+    if (err) return console.error(err);
+    console.log("Copy website to _site");
+  });
+
+  fs.copy("examples", "_site/examples", function(err) {
+    if (err) return console.error(err);
+    console.log("Success: examples copied to website directory");
+  });
+} catch (err) {
+  console.error(err);
+}
